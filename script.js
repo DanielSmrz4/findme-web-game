2a30506240a688f4b24aeb382593ae92e3268a9a
Smrž Daniel <dsmrz@trask.cz>
2024-10-10 21:00:25 +0200

    v1.1 counting added

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
+// message when win
 let count = document.querySelector("#count")
 
+// attemps count
+const attemps = document.querySelector(".attemps")
+let startCountingFrom = 0
+attemps.innerText = startCountingFrom
+
+// array of all tiles
 let allTiles = []
 allTiles = document.querySelectorAll(".one-tile-js")
 
+// selecting randomly winner tile
 const myNum = Math.floor(Math.random() * 9)
 console.log(myNum)
-
 const winnerTile = allTiles[myNum]
 winnerTile.classList.remove("one-tile-js")
 winnerTile.classList.add("winner-indicator")
 
+// adding click e-listener for each tile
 allTiles.forEach((e) => {
         if (e.classList[1] == "one-tile-js") {
             e.addEventListener("click", (e) => {
                 e.preventDefault()
-                e.target.classList.add("wrong-tile")      
+                e.target.classList.add("wrong-tile")
+                if (startCountingFrom < 9) {
+                    startCountingFrom += 1   
+                    attemps.innerText = startCountingFrom
+                }
+                   
             })
         } else if (e.classList[1] == "winner-indicator") {
             e.addEventListener("click", (e) => {
                 e.preventDefault()
+                if (startCountingFrom < 9) {
+                    startCountingFrom += 1   
+                    attemps.innerText = startCountingFrom
+                }
                 count.innerText = ("FUCK YEAH!!!")
                 winnerTile.classList.add("winner-highligh")
             })
         }                
 });
 
+// reloads page - reset button functionality
 const resetButton = document.querySelector(".reset-btn")
 resetButton.addEventListener("click", (e) => {
     location.reload()
